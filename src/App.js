0bec5fb312c5d1dfdc9c7c7f4ffa2a6b1a3e08c4
vidy <vidy.alfredo1@gmail.com>
2022-12-26 15:34:36 +0800

    add button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useCallback,  useRef } from 'react';
 
 function App() {
+
+  const inputRef = useRef();
+
+
+
+  const handleRunTraining= useCallback((e) => {
+
+    console.log('Run training');
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Fitness Assistant
       </header>
+      <input  type={"file"} style={{visibility: 'hidden'}} ref={inputRef}/>
+      <button onClick={handleRunTraining}>Run training</button>
     </div>
   );
 }

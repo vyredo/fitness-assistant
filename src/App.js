4ce617a10a29107ceb7b827f63a1e0dfae4a4c6d
vidy <vidy.alfredo1@gmail.com>
2022-12-26 16:13:11 +0800

    Fitness Assistant P1 M4
    - create function handleRunTraning that create model
    - create function doTraining
    - after training, pass the input [10] to prediction
    - prediction is usually closer to 19

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 
 import './App.css';
-import { useCallback,  useRef } from 'react';
+import { useCallback,   useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
 
 function App() {
 
   const inputRef = useRef();
+  const modelRef = useRef( tf.sequential({
+    // Define a one-layer sequential model in the handleRunTraining function.
+    // Use inputShape = 1 and 1 unit for the result.
+    layers: [tf.layers.dense({units: 1, inputShape: [1]})]
+  }));
+
+  const doTraining = useCallback(async (input, target) => {
+    const m = modelRef.current
+    const h = await m?.fit(input, target, {
+      epochs: 200
+    })
+    // statistic onEpochEnd
+    h.onEpochEnd(epoch => {
+      console.log(`Epoch ${epoch}: loss = ${h.history.loss[epoch]}`);
+    })
+  },[])
+
+  const handleRunTraining= useCallback(async (e) => {
+    // Compile the model with the SGD optimizer and meanSquaredError loss.
+    // Output the model summary. (what does it mean to outpout the model summary?)
+    modelRef.current.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+
+    
+    const input = tf.tensor2d([2, 3, 4, 5, 6, 7], [6, 1]);
+    const target = tf.tensor2d([3, 5, 7, 9, 11, 13], [6, 1]);
+    doTraining(input, target)
 
 
-
-  const handleRunTraining= useCallback((e) => {
-
-    console.log('Run training');
-  }, [])
+    // pass value 10, and print the result of close to 19 
+    // the result is usually > 18.8xx
+    modelRef.current.predict(tf.tensor2d([10], [1,1])).print()
+  }, [doTraining])
 
 
   return (

fc9e980f88080801f584a141977555d1c978649d
vidy <vidy.alfredo1@gmail.com>
2022-12-27 08:45:16 +0800

    Fitness-Assistant-p2-m2 - add react-webcam - integrate and render webcam into webpage

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 
 import './App.css';
-import { useCallback,   useRef } from 'react';
-import * as tf from '@tensorflow/tfjs';
+import { useCallback,   useEffect,   useRef, useState } from 'react';
+import '@tensorflow/tfjs-backend-webgl';
+import '@tensorflow/tfjs-backend-webgpu';
+// posenet is deprecated, import pose-detection instead
+import * as posedetection from '@tensorflow-models/pose-detection';
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 800,
+  height: 600,
+  facingMode: "user"
+};
+
 
 function App() {
 
-  const inputRef = useRef();
-  const modelRef = useRef( tf.sequential({
-    // Define a one-layer sequential model in the handleRunTraining function.
-    // Use inputShape = 1 and 1 unit for the result.
-    layers: [tf.layers.dense({units: 1, inputShape: [1]})]
-  }));
 
-  const doTraining = useCallback(async (input, target) => {
-    const m = modelRef.current
-    const h = await m?.fit(input, target, {
-      epochs: 200
-    })
-    // statistic onEpochEnd
-    h.onEpochEnd(epoch => {
-      console.log(`Epoch ${epoch}: loss = ${h.history.loss[epoch]}`);
-    })
+  const [model, setModel] = useState();
+  const webcamRef = useRef(null);
+  const loadPosenet = useCallback(async () => {
+    const posenet = posedetection.SupportedModels.PoseNet;
+    const result = await posedetection.createDetector(posenet, {
+      quantBytes: 4,
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      inputResolution: {width: 800, height: 600},
+      multiplier: 0.75
+    });
+    setModel(result)
   },[])
+  
+  useEffect(() => {
+    if(!model){
+      loadPosenet()
+    }
 
-  const handleRunTraining= useCallback(async (e) => {
-    // Compile the model with the SGD optimizer and meanSquaredError loss.
-    // Output the model summary. (what does it mean to outpout the model summary?)
-    modelRef.current.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
-
-    
-    const input = tf.tensor2d([2, 3, 4, 5, 6, 7], [6, 1]);
-    const target = tf.tensor2d([3, 5, 7, 9, 11, 13], [6, 1]);
-    doTraining(input, target)
-
-
-    // pass value 10, and print the result of close to 19 
-    // the result is usually > 18.8xx
-    modelRef.current.predict(tf.tensor2d([10], [1,1])).print()
-  }, [doTraining])
+    console.log('Posenet Model Loaded..')
+    console.log(model)
+  },[loadPosenet, model] )
 
 
   return (
@@ -45,8 +46,34 @@ function App() {
       <header className="App-header">
         Fitness Assistant
       </header>
-      <input  type={"file"} style={{visibility: 'hidden'}} ref={inputRef}/>
-      <button onClick={handleRunTraining}>Run training</button>
+      <Webcam
+      ref={webcamRef}
+      style={{
+        position: "absolute",
+        marginLeft: "auto",
+        marginRight: "auto",
+        left: 0,
+        right: 0,
+        textAlign: "center",
+        zindex: 9
+      }}
+        audio={false}
+        height={600}
+        screenshotFormat="image/jpeg"
+        width={800}
+        videoConstraints={videoConstraints}
+      >
+        {({ getScreenshot }) => (
+          <button
+            onClick={() => {
+              const imageSrc = getScreenshot()
+              console.log(imageSrc)
+            }}
+          >
+            Capture photo
+          </button>
+        )}
+      </Webcam>
     </div>
   );
 }

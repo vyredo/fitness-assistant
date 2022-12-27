cbc009bf51dc0031c6eb5cad0c429d7df926cd9b
vidy <vidy.alfredo1@gmail.com>
2022-12-27 09:32:51 +0800

    Fitness Assistant p2 m3
    - adding functions to start pose estimation with interval 100ms
    - logs the pose estimation result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { useCallback,   useEffect,   useRef, useState } from 'react';
+import React, { useCallback,   useEffect,   useRef, useState } from 'react';
 import '@tensorflow/tfjs-backend-webgl';
 import '@tensorflow/tfjs-backend-webgpu';
 // posenet is deprecated, import pose-detection instead
@@ -14,11 +14,15 @@ const videoConstraints = {
 };
 
 
+
+
 function App() {
-
-
   const [model, setModel] = useState();
+  const [isPoseEstimation, setIsPoseEstimation] = useState(false)
+
   const webcamRef = useRef(null);
+  
+  
   const loadPosenet = useCallback(async () => {
     const posenet = posedetection.SupportedModels.PoseNet;
     const result = await posedetection.createDetector(posenet, {
@@ -29,51 +33,71 @@ function App() {
       multiplier: 0.75
     });
     setModel(result)
+
+    // result.estimatePoses
   },[])
-  
   useEffect(() => {
     if(!model){
       loadPosenet()
     }
-
+    
     console.log('Posenet Model Loaded..')
     console.log(model)
   },[loadPosenet, model] )
+  
+  
+  
+  const poseEstimationLoop = useRef(0)
+  const startPoseEstimation = useCallback(() => {
+    if(!webcamRef.current) return
 
+    poseEstimationLoop.current = window.setInterval(async () => {
+      const video = webcamRef.current?.video
+      const { videoWidth, videoHeight } = video;
+
+      const pose = await model?.estimatePoses(video, {
+        flipHorizontal: false,
+        decodingMethod: 'single-person'
+      })
+      console.log(pose)
+    },100)
+  }, [model])
+  const stopPoseEstimation = useCallback(() => window.clearInterval(poseEstimationLoop.current),[])
+  const handlePoseEstimation = useCallback(() =>{
+    if(isPoseEstimation){
+      stopPoseEstimation()
+      setIsPoseEstimation(false)
+    }  else {
+      startPoseEstimation()
+      setIsPoseEstimation(true)
+    }
+  }, [isPoseEstimation, startPoseEstimation, stopPoseEstimation])
 
   return (
     <div className="App">
       <header className="App-header">
         Fitness Assistant
       </header>
+      <button onClick={handlePoseEstimation}>
+        {isPoseEstimation ? 'Stop': 'Start'}
+      </button>
       <Webcam
       ref={webcamRef}
       style={{
         position: "absolute",
         marginLeft: "auto",
         marginRight: "auto",
+        marginTop: 30,
         left: 0,
         right: 0,
         textAlign: "center",
-        zindex: 9
       }}
         audio={false}
         height={600}
         screenshotFormat="image/jpeg"
         width={800}
         videoConstraints={videoConstraints}
-      >
-        {({ getScreenshot }) => (
-          <button
-            onClick={() => {
-              const imageSrc = getScreenshot()
-              console.log(imageSrc)
-            }}
-          >
-            Capture photo
-          </button>
-        )}
-      </Webcam>
+      />
     </div>
   );
 }
